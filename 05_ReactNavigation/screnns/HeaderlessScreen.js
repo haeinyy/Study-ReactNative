a3be43f0f0598485d10a632a98cb007532701a23
haeinyy <godls4402@gmail.com>
2026-05-04 18:38:19 +0900

    feat. StackNavigator

---
+++ 05_ReactNavigation/screnns/HeaderlessScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+function HeaderlessScreen({navigation}) {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>헤더가 없는 화면입니다.</Text>
+        <Button title="뒤로가기" onPress={() => navigation.pop()} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default HeaderlessScreen;
